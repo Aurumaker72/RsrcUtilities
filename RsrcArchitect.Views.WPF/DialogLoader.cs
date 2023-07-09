3950863708a34d36649ebe4068aab40b1b159e28
Alex <48759429+Aurumaker72@users.noreply.github.com>
2023-07-09 13:20:39 +0200

    Add runtime control generation for all  controls

--- RsrcArchitect.Views.WPF/DialogLoader.cs
+++ RsrcArchitect.Views.WPF/DialogLoader.cs
@@ -11,34 +11,79 @@ public static class DialogLoader
     private const string ClassName = "previewWindow";
     private static HINSTANCE _instance;
 
+    // ReSharper disable InconsistentNaming
+    // ReSharper disable IdentifierTypo
+    private const long ES_AUTOHSCROLL = 0x0080L;
+    private const long ES_READONLY = 0x0800L;
+    private const long BS_AUTOCHECKBOX = 0x00000003L;
+    private const long BS_GROUPBOX = 0x00000007L;
+    private const long CBS_DROPDOWN = 0x0002L;
+    private const long CBS_DROPDOWNLIST = 0x0003L;
+    // ReSharper restore InconsistentNaming
+    // ReSharper restore IdentifierTypo
 
     private static unsafe void CreateControl(HWND dialogHwnd, Control control, Rectangle rectangle)
     {
         if (control is Button button)
         {
-            PInvoke.CreateWindowEx(0L, "BUTTON".ToPcwstr(), button.Caption.ToPcwstr(),
-                WINDOW_STYLE.WS_BORDER | WINDOW_STYLE.WS_TABSTOP | WINDOW_STYLE.WS_CHILD | (WINDOW_STYLE)0x00000001L, rectangle.X, rectangle.Y,
-                rectangle.Width, rectangle.Height, dialogHwnd, HMENU.Null,
-                _instance);
+            PInvoke.CreateWindowEx(0, "BUTTON".ToPcwstr(), button.Caption.ToPcwstr(),
+                WINDOW_STYLE.WS_VISIBLE | WINDOW_STYLE.WS_CHILD, rectangle.X, rectangle.Y, rectangle.Width,
+                rectangle.Height, dialogHwnd, HMENU.Null, _instance);
+        }
+        else if (control is TextBox textBox)
+        {
+            var style = WINDOW_STYLE.WS_VISIBLE | WINDOW_STYLE.WS_CHILD;
+
+            if (!textBox.IsWriteable) style |= (WINDOW_STYLE)ES_READONLY;
+            if (textBox.AllowHorizontalScroll) style |= (WINDOW_STYLE)ES_AUTOHSCROLL;
+
+            PInvoke.CreateWindowEx(WINDOW_EX_STYLE.WS_EX_CLIENTEDGE, "EDIT".ToPcwstr(), "".ToPcwstr(),
+                style, rectangle.X, rectangle.Y, rectangle.Width,
+                rectangle.Height, dialogHwnd, HMENU.Null, _instance);
+        }
+        else if (control is CheckBox checkBox)
+        {
+            PInvoke.CreateWindowEx(0, "BUTTON".ToPcwstr(), checkBox.Caption.ToPcwstr(),
+                WINDOW_STYLE.WS_VISIBLE | WINDOW_STYLE.WS_CHILD | (WINDOW_STYLE)BS_AUTOCHECKBOX, rectangle.X,
+                rectangle.Y, rectangle.Width,
+                rectangle.Height, dialogHwnd, HMENU.Null, _instance);
+        }
+        else if (control is GroupBox groupBox)
+        {
+            PInvoke.CreateWindowEx(0, "BUTTON".ToPcwstr(), groupBox.Caption.ToPcwstr(),
+                WINDOW_STYLE.WS_VISIBLE | WINDOW_STYLE.WS_CHILD | (WINDOW_STYLE)BS_GROUPBOX, rectangle.X, rectangle.Y,
+                rectangle.Width,
+                rectangle.Height, dialogHwnd, HMENU.Null, _instance);
+        }
+        else if (control is ComboBox comboBox)
+        {
+            PInvoke.CreateWindowEx(0, "ComboBox".ToPcwstr(), "?".ToPcwstr(),
+                WINDOW_STYLE.WS_VISIBLE | WINDOW_STYLE.WS_CHILD | (WINDOW_STYLE)CBS_DROPDOWN | (WINDOW_STYLE)CBS_DROPDOWNLIST, rectangle.X, rectangle.Y,
+                rectangle.Width,
+                rectangle.Height, dialogHwnd, HMENU.Null, _instance);
+        }
+        else if (control is Label label)
+        {
+            PInvoke.CreateWindowEx(0, "STATIC".ToPcwstr(), label.Caption.ToPcwstr(),
+                WINDOW_STYLE.WS_VISIBLE | WINDOW_STYLE.WS_CHILD, rectangle.X, rectangle.Y, rectangle.Width,
+                rectangle.Height, dialogHwnd, HMENU.Null, _instance);
         }
     }
 
     public static unsafe void ShowDialogFromRcString(DialogViewModel dialogViewModel)
     {
-        var wc = new WNDCLASSEXW();
-        MSG msg;
-        HWND hwnd;
-
         _instance = PInvoke.GetModuleHandle((PCWSTR)null);
 
-        wc.cbSize = 80U;
-        wc.lpfnWndProc = PInvoke.DefWindowProc;
-        wc.hInstance = _instance;
-        wc.lpszClassName = ClassName.ToPcwstr();
+        var wc = new WNDCLASSW
+        {
+            lpfnWndProc = PInvoke.DefWindowProc,
+            hInstance = _instance,
+            lpszClassName = ClassName.ToPcwstr()
+        };
+        PInvoke.RegisterClass(in wc);
 
-        PInvoke.RegisterClassEx(in wc);
 
-        hwnd = PInvoke.CreateWindowEx(0,
+        var hwnd = PInvoke.CreateWindowEx(0,
             ClassName.ToPcwstr(),
             dialogViewModel.Caption.ToPcwstr(),
             (WINDOW_STYLE)(0x00000000L | 0x00C00000L | 0x00080000L | 0x00040000L | 0x00020000L | 0x00010000L),
@@ -51,13 +96,10 @@ public static class DialogLoader
             _instance);
 
         PInvoke.ShowWindow(hwnd, SHOW_WINDOW_CMD.SW_SHOW);
-        PInvoke.UpdateWindow(hwnd);
 
-        foreach (var (key, value) in dialogViewModel.DoLayout())
-        {
-            CreateControl(hwnd, key, value);
-        }
+        foreach (var (key, value) in dialogViewModel.DoLayout()) CreateControl(hwnd, key, value);
 
+        MSG msg;
         while (PInvoke.GetMessage(&msg, hwnd, 0, 0))
         {
             PInvoke.TranslateMessage(&msg);
